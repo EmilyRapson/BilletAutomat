0fcb2313a6b739f44f38fc3b3c30c7aff73ae072
EmilyRapson <emilierapson@hotmail.com>
2019-03-23 12:45:59 +0100

    2. commit

--- src/BilletAutomat/BilletAutomat.java
+++ src/BilletAutomat/BilletAutomat.java
@@ -2,4 +2,41 @@ package BilletAutomat;
 
 public class BilletAutomat {
 
+private int balance;
+private int billetpris;
+private int solgtebilletter;
+
+public BilletAutomat()
+{
+
+}
+
+
+public BilletAutomat(int billetpris)
+{
+this.billetpris = billetpris;
+}
+
+
+public BilletAutomat (int billetpris, int startbalance)
+{
+this.billetpris = billetpris;
+this.balance = startbalance;
+}
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void setBilletpris(int billetpris) {
+        this.billetpris = billetpris;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int getBilletpris() {
+        return billetpris;
+    }
 }
---
+++ src/BilletAutomat/Liste.java
@@ -0,0 +1,9 @@
+package BilletAutomat;
+
+import java.util.ArrayList;
+
+public interface Liste {
+
+public void addBillet (BilletAutomat billet);
+
+}
